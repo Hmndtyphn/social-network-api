37c94ded5da2cade27ea112b0862b282e14a21d1
HmndTyphn <Jared.elliott09@gmail.com>
2021-12-06 16:37:52 -0600

    models basic structure

---
+++ models/Comments.js
@@ -0,0 +1,4 @@
+const { Schema, model, Types} = require('mongoose');
+const moment = require('moment');
+
+module.exports = Comments;
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email for login']
+    },
+})
