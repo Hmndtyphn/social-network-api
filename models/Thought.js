f88f3bcc7bc0c7c473fba4141f5e5de2ac6cd525
HmndTyphn <Jared.elliott09@gmail.com>
2021-12-07 17:16:42 -0600

    reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,37 @@
 const { Schema, model, Types} = require('mongoose');
 const moment = require('moment');
 
+const reactionSchema = new Schema(
+    {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+       type: String,
+       required: true,
+       trim: true,
+       minLength: 1,
+       maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MM DD, YY [at] hh:mm a')
+    }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+
 
 
 module.exports = Thought;
