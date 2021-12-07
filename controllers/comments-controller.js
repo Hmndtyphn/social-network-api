d4c3c53a19541004aefe65c7bb496b6543192eec
HmndTyphn <Jared.elliott09@gmail.com>
2021-12-07 13:47:13 -0600

    get all comments route added for testing

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,1 +1,21 @@
-const { Comments, User }
+const { Comments, User } = require('../models');
+
+const commentsController = {
+    // get all comments route
+    getAllComments(req, res) {
+        Comments.find({})
+        .populate({
+            path: 'user',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbCommentsData => res.json(dbCommentsData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    },
+
+    
+}
