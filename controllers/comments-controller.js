4d8ddea214ebeb46b825bdc1f9d7f26823e7ce21
HmndTyphn <Jared.elliott09@gmail.com>
2021-12-07 14:17:07 -0600

    get comment by id/ create comments

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -17,5 +17,42 @@ const commentsController = {
         });
     },
 
+    // get single comment by id
+    getCommentsById({ params }, res) {
+        Comments.findOne({ _id: params.id })
+        .populate({
+            path: 'user',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbCommentsData => res.json(dbCommentsData))
+        .catch( err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
+    },
+
+    // create comments
+    createComments({ params, body }, res) {
+        Comments.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { username: body.username },
+                { $push: {comments: _id } },
+                { new: true }
+            );
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
     
+
 }
